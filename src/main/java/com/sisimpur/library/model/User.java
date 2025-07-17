1183daacf728e57cf514a902ca2a09d9baa341b5
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2025-07-17 06:56:41 +0600

    add: user model

---
+++ src/main/java/com/sisimpur/library/model/User.java
@@ -0,0 +1,30 @@
+package com.sisimpur.library.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", nullable = false, length = 100, unique = true)
+    private String email;
+}
