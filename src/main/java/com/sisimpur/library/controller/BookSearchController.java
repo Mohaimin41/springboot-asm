f2d9c2fc4c6c3cf70780531d208a18a82f3e755f
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2025-07-17 11:15:45 +0600

    feat: book search routes

---
+++ src/main/java/com/sisimpur/library/controller/BookSearchController.java
@@ -0,0 +1,32 @@
+package com.sisimpur.library.controller;
+
+import com.sisimpur.library.dto.BookDTO;
+import com.sisimpur.library.service.BookSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/book-search")
+@RequiredArgsConstructor
+public class BookSearchController {
+
+    private final BookSearchService bookSearchService;
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> searchBooks(
+            @RequestParam(required = false) String author_name,
+            @RequestParam(required = false) String book_name,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) Integer publishing_year
+    ) {
+        return ResponseEntity.ok(
+                bookSearchService.searchBooks(author_name, book_name, category, publishing_year)
+        );
+    }
+}
